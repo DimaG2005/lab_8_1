7a497941ae2587cbf143e2988bebdfbe737267d5
Dima Horobets <152785375+DimaG2005@users.noreply.github.com>
2023-12-03 21:14:28 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class ConfigurationManager
+{
+    private static ConfigurationManager instance = null;
+    private static readonly object padlock = new object();
+
+    public string LoggingMode { get; set; }
+    public string DatabaseConnectionSettings { get; set; }
+
+    private ConfigurationManager()
+    {
+        // Приватний конструктор, щоб запобігти створенню екземплярів ззовні класу
+    }
+
+    public static ConfigurationManager Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new ConfigurationManager();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public void ChangeSettings(string loggingMode, string databaseConnectionSettings)
+    {
+        LoggingMode = loggingMode;
+        DatabaseConnectionSettings = databaseConnectionSettings;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConfigurationManager configManager = ConfigurationManager.Instance;
+
+        Console.WriteLine("Enter logging mode:");
+        string loggingMode = Console.ReadLine();
+
+        Console.WriteLine("Enter database connection settings:");
+        string databaseConnectionSettings = Console.ReadLine();
+
+        configManager.ChangeSettings(loggingMode, databaseConnectionSettings);
+
+        Console.WriteLine("Logging mode: " + configManager.LoggingMode);
+        Console.WriteLine("Database connection settings: " + configManager.DatabaseConnectionSettings);
+
+        Console.ReadKey();
+    }
+}
